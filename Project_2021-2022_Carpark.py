425d9226c48655998f40342b330d5ae9a00e9ce6
M-janssens <r0841955@student.thomasmore.be>
2021-11-24 13:02:45 +0100

    camera

--- Project_2021-2022_Carpark.py
+++ Project_2021-2022_Carpark.py
@@ -36,3 +36,12 @@ def stepdrive(pin1,pin2,pin3,pin4):
     GPIO.output(pin3, 0)
     GPIO.output(pin4, 1)
     time.sleep(0.01)
+
+
+def photo():
+    camera = PiCamera() # TODO deze rotatie kan nog aangepast worden A.D.H.V. hoe de camera geposisioneert staat
+    camera.rotation = 180
+    camera.start_preview()
+    sleep(2)
+    camera.capture('/home/pi/images/photo.jpg')  # TODO make location for the pictures
+    camera.stop_preview()
