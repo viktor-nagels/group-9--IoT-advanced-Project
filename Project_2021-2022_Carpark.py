e101ab0de362aca1236d6cd03d804e67ac5f3391
Viktor Nagels <71647271+viktor-nagels@users.noreply.github.com>
2021-11-24 17:22:06 +0100

    banaan

--- Project_2021-2022_Carpark.py
+++ Project_2021-2022_Carpark.py
@@ -11,6 +11,7 @@ pinsoon2 = 21
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pinsoon1, GPIO.OUT)
 GPIO.setup(pinsoon2, GPIO.IN)
+GPIO.setup((6,13,19,26), GPIO.OUT)
 
 def stepdrive(pin1,pin2,pin3,pin4):  
     pinnumbers = pin1,pin2,pin3,pin4
@@ -39,7 +40,7 @@ def stepdrive(pin1,pin2,pin3,pin4):
 
 def photo():
     camera = PiCamera() # TODO deze rotatie kan nog aangepast worden A.D.H.V. hoe de camera geposisioneert staat
-    camera.rotation = 180
+    #camera.rotation = 0
     camera.start_preview()
     sleep(2)
     camera.capture('/home/pi/images/photo.jpg')  # TODO make location for the pictures
@@ -54,8 +55,8 @@ def numberplate():
             files=dict(upload=fp),
             headers={'Authorization': 'Token 315a9c42be797329049bf2cc52a5cb41ab960e15'})
     json_results = (response.json())
-    numberplate = (json_results['results'][0]['plate'])
-    return numberplate
+    plate = (json_results['results'][0]['plate'])
+    return plate
 
 
 while True:
@@ -76,18 +77,23 @@ while True:
   
     if distance <= 30 :    #in centimeter
         photo()
-        print('AUTO aan de bareel')
-        for n in range(0, 130):
-                stepdrive(23,24,25,26)
-        time.sleep(30)   # moet nog veranderd worden naar als de auto weg is
-        
-        for n in range(0, 130):
-                stepdrive(26,25,24,23)
+        sleep(5)
+        numberplate()
+        numberplate = numberplate()
+        print(numberplate)
+        if numberplate is str: #TODO zoeken hoe we foute nummerplaat kunnen herkennen
+            print('AUTO aan de bareel')
+            for n in range(0, 130):
+                    stepdrive(6,13,19,26)
+            time.sleep(5)   # moet nog veranderd worden naar als de auto weg is
+            
+            for n in range(0, 130):
+                    stepdrive(26,19,13,6)
         
     else:
-        GPIO.output(23, 0)
-        GPIO.output(24, 0)
-        GPIO.output(25, 0)
+        GPIO.output(6, 0)
+        GPIO.output(13, 0)
+        GPIO.output(19, 0)
         GPIO.output(26, 0)
         print('GEEN auto aan de bareel')
         time.sleep(0.5)
