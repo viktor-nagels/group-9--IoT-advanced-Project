e7de68a5a558dfb5d307c312be643fe6eb400653
Viktor Nagels <71647271+viktor-nagels@users.noreply.github.com>
2021-11-24 13:04:17 +0100

    1

--- numberplate_recognition.py
+++ numberplate_recognition.py
@@ -1,12 +1,14 @@
 import requests
-from pprint import pprint
-regions = ['mx', 'be'] # Change to your country
-with open('/home/pi/images/photo.jpg', 'rb') as fp:
-    response = requests.post(
-        'https://api.platerecognizer.com/v1/plate-reader/',
-        data=dict(regions=regions),  # Optional
-        files=dict(upload=fp),
-        headers={'Authorization': 'Token 315a9c42be797329049bf2cc52a5cb41ab960e15'})
-json_results = (response.json())
-numberplate = (json_results['results'][0]['plate'])
+def numberplate():
+    regions = ['mx', 'be'] # Change to your country
+    with open('/home/pi/images/photo.jpg', 'rb') as fp:
+        response = requests.post(
+            'https://api.platerecognizer.com/v1/plate-reader/',
+            data=dict(regions=regions),  # Optional
+            files=dict(upload=fp),
+            headers={'Authorization': 'Token 315a9c42be797329049bf2cc52a5cb41ab960e15'})
+    json_results = (response.json())
+    numberplate = (json_results['results'][0]['plate'])
+    return numberplate
+numberplates = numberplate()
 print(numberplate)
