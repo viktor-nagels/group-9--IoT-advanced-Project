b6c9a4c0d1504cbe16732fe753d4f9d4e5cd2fd0
Viktor Nagels <71647271+viktor-nagels@users.noreply.github.com>
2021-11-24 13:07:04 +0100

    update

--- Project_2021-2022_Carpark.py
+++ Project_2021-2022_Carpark.py
@@ -45,3 +45,17 @@ def photo():
     sleep(2)
     camera.capture('/home/pi/images/photo.jpg')  # TODO make location for the pictures
     camera.stop_preview()
+
+def numberplate():
+    regions = ['mx', 'be'] # Change to your country
+    with open('/home/pi/images/photo.jpg', 'rb') as fp:
+        response = requests.post(
+            'https://api.platerecognizer.com/v1/plate-reader/',
+            data=dict(regions=regions),  # Optional
+            files=dict(upload=fp),
+            headers={'Authorization': 'Token 315a9c42be797329049bf2cc52a5cb41ab960e15'})
+    json_results = (response.json())
+    numberplate = (json_results['results'][0]['plate'])
+    return numberplate
+
+
