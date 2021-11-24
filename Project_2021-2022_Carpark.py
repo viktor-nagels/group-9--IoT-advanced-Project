aa78fcae23bccf2df7e7d23497620d27e2f6e554
M-janssens <r0841955@student.thomasmore.be>
2021-11-24 12:56:11 +0100

    start

---
+++ Project_2021-2022_Carpark.py
@@ -0,0 +1,38 @@
+from picamera import PiCamera
+from time import sleep
+import requests
+from pprint import pprint
+import RPi.GPIO as GPIO
+import time
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(18, GPIO.IN)
+
+
+
+def stepdrive(pin1,pin2,pin3,pin4):  
+    pinnumbers = pin1,pin2,pin3,pin4
+    GPIO.setup(pinnumbers, GPIO.OUT)
+    GPIO.output(pinnumbers, 0)
+    GPIO.output(pin1, 1)
+    GPIO.output(pin2, 1)
+    GPIO.output(pin3, 0)
+    GPIO.output(pin4, 0)
+    time.sleep(0.01)
+    GPIO.output(pin1, 0)
+    GPIO.output(pin2, 1)
+    GPIO.output(pin3, 1)
+    GPIO.output(pin4, 0)
+    time.sleep(0.01)
+    GPIO.output(pin1, 0)
+    GPIO.output(pin2, 0)
+    GPIO.output(pin3, 1)
+    GPIO.output(pin4, 1)
+    time.sleep(0.01)
+    GPIO.output(pin1, 1)
+    GPIO.output(pin2, 0)
+    GPIO.output(pin3, 0)
+    GPIO.output(pin4, 1)
+    time.sleep(0.01)
