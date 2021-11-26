466c46216296c5b1f2744e34d24f9449bf8de1b1
Bent Melis <71631709+bmelis@users.noreply.github.com>
2021-11-26 01:10:18 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -12,46 +12,29 @@
 	$hostname = "localhost";
 	$username = "pi";
 	$password = "raspberry";
-	$db = "IoT-Advanced-Project";
+	$db = "ParkingDB";
 
 	$dbconnect=mysqli_connect($hostname,$username,$password,$db);
 
 	if ($dbconnect->connect_error) {
 		die("Database connection failed: " . $dbconnect->connect_error);
 	}
-	?>
-	<?php
 
-	$query = mysqli_query($dbconnect, "SELECT StatusParkingLot FROM `parkingLots`")
+	$query = mysqli_query($dbconnect, "SELECT bezet FROM `ParkingLot`")
 		or die (mysqli_error($dbconnect));
 	while ($row = mysqli_fetch_array($query)) {
-		$StatusParkingLot[] = $row[0];   
+		$bezet[] = $row[0];
 	}
-    
 	$rood = '#E16950';
 	$groen = '#91F160';
 	?>
-
     <h1>
         Parking webinterface
     </h1>
     <svg width="150" height="360">
         <rect id="parking1" x="0" y="0" width="150" height="360" style="fill:
 		<?php
-            if($StatusParkingLot[0] == 0){
-                echo $groen;
-            }
-            else{
-                echo $rood;
-            }
-        ?>
-		;stroke:black;stroke-width:5;" />
-
-    </svg>    
-	<svg width="150" height="360">
-        <rect id="parking1" x="0" y="0" width="150" height="360" style="fill:
-		<?php
-            if($StatusParkingLot[1] == 0){
+            if($bezet[0] == 0){
                 echo $groen;
             }
             else{
@@ -63,7 +46,7 @@
 	<svg width="150" height="360">
         <rect id="parking1" x="0" y="0" width="150" height="360" style="fill:
 		<?php
-            if($StatusParkingLot[2] == 0){
+            if($bezet[1] == 0){
                 echo $groen;
             }
             else{
@@ -75,7 +58,19 @@
 	<svg width="150" height="360">
         <rect id="parking1" x="0" y="0" width="150" height="360" style="fill:
 		<?php
-            if($StatusParkingLot[3] == 0){
+            if($bezet[2] == 0){
+                echo $groen;
+            }
+            else{
+                echo $rood;
+            }
+        ?>
+		;stroke:black;stroke-width:5;" />
+    </svg>    
+	<svg width="150" height="360">
+        <rect id="parking1" x="0" y="0" width="150" height="360" style="fill:
+		<?php
+            if($bezet[3] == 0){
                 echo $groen;
             }
             else{
@@ -86,14 +81,26 @@
     </svg>
 	<div> Aantal vrije plaatsen: <span style="font-weight:bold">
 	<?php
-	header("refresh: 3");
-	$counts = array_count_values($StatusParkingLot);
-    if($counts['0'] == 0){
-        echo '0';
-    } else{
-	    echo  $counts['0'];
-    }
+	$counts = array_count_values($bezet);
+	echo  $counts['0'] 
 	?></span>
 	</div>
+	<?php
+	if($counts['0'] == 0){
+		curl_setopt_array($ch = curl_init(), array(
+		CURLOPT_URL => "https://api.pushover.net/1/messages.json",
+		CURLOPT_POSTFIELDS => array(
+			"token" => "adcxfe2q99fibaupf4arwf3cdi7sa4",
+			"user" => "uabtbvyuxaudjddqp86o5rh5xordta",
+			"message" => "Youre parking is full! 🚗",
+		),
+		CURLOPT_SAFE_UPLOAD => true,
+		CURLOPT_RETURNTRANSFER => true,
+		));
+		curl_exec($ch);
+		curl_close($ch);
+	}
+	header("refresh: 3");
+	?>
 </body>
 </html>
